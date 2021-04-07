f18673132f8ca9e640ac9f78130271ccf7246e61
Youngjin Kim <steven.kimijin@gmail.com>
2021-04-07 23:52:22 +0900

    update fibonacci_number

---
+++ java-correct/fibonacci_number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        return fibonacci(n);
+    }
+    
+    public int fibonacci(int n){
+        if(n == 0) return 0;
+        else if(n == 1) return 1;
+        else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+}
