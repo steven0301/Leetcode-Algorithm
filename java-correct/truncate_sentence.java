e5e7be6e4632806b0d5816133a0d3bead93138ea
steven <steven.kimijin@gmail.com>
2021-04-08 23:52:23 +0900

    update truncate_sentence

---
+++ java-correct/truncate_sentence.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        String[] words = s.split(" ");
+        for(int i=0; i<k ;i++){
+            sb.append(words[i]);
+            if(i != k-1) sb.append(" ");
+        }
+        return sb.toString();
+    }
+}
